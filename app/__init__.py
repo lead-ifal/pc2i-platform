47c192279ccc70175a0115a9ed6e8fec0e87ae78
Filipe Alves <filipe.alvesdsouza@gmail.com>
2021-11-05 20:37:17 -0300

    Register users routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,14 @@
 from config import Config
 from flask import Flask
+from flask_pymongo import PyMongo
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config.from_object(Config)
+CORS(app)
+
+pymongo_client = PyMongo(app)
+database = pymongo_client.db
+
+from .routes.users import users_routes as users_blueprint
+app.register_blueprint(users_blueprint)
