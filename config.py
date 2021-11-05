62b44164d2db5b5f4d2c2145443c0f5723d57b12
Filipe Alves <filipe.alvesdsouza@gmail.com>
2021-11-05 20:21:28 -0300

    Update config.py with environment variables

--- config.py
+++ config.py
@@ -1,18 +1,8 @@
 import os
+import dotenv
+
+dotenv.load_dotenv(dotenv.find_dotenv())
 
 class Config:
-
-    SECRET_KEY = os.urandom(32)
-
-    # Recupera o diretório onde o script irá ser executado.
-    basedir = os.path.abspath(os.path.dirname(__file__))
-
-    # Ativa o modo debug.
-    DEBUG = True
-
-    # Connect to the database
-    USER = "pc2idev"
-    PASSWORD = ""
-    HOST = "localhost"
-    MONGO_DATABASE = 'pc2idev'
-    MONGO_URI = "mongodb+srv://{}:{}@cluster0.vdufl.mongodb.net/{}?retryWrites=true&w=majority".format(USER,PASSWORD,MONGO_DATABASE)
+  DEBUG = True
+  MONGO_URI = os.getenv('MONGO_URI')
