6f18c309c9a73cf32949706db87e2aabc604231a
Filipe Alves <filipe.alvesdsouza@gmail.com>
2022-02-03 10:38:04 -0300

    Add and use zone id attribute

--- app/models/zone.py
+++ app/models/zone.py
@@ -1,6 +1,7 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class Zone(BaseModel):
+  id: int = Field(alias="_id")
   name: str
   description: str
   size: float
--- app/routes/zones/__init__.py
+++ app/routes/zones/__init__.py
@@ -4,6 +4,7 @@ zones_routes = Blueprint('zones', __name__)
 
 from flask import request
 from app.controllers.global_controller import GlobalController
+from app.controllers.zone_controller import ZoneController
 from app.models.zone import Zone
 from typing import Collection
 from app import database
@@ -19,11 +20,14 @@ def create():
 
   if includesParams:
     try:
+      body['_id'] = ZoneController.buildZoneId(zones)
       zone = Zone(**body)
-      zones.insert_one(zone.dict())
+      zoneData = zone.dict(by_alias=True)
+
+      zones.insert_one(zoneData)
 
       message = 'Zone de irrigação criada com sucesso'
-      return GlobalController.generateResponse(200, message, body)
+      return GlobalController.generateResponse(200, message, zoneData)
     except:
       errorMessage = 'Não foi possível criar uma zona de irrigação com os dados informados'
       return GlobalController.generateResponse(400, errorMessage)
