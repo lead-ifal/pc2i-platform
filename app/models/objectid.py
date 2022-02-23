06226038e60dc840ab7c5c86b1acb3554c7358b1
Filipe Alves <filipe.alvesdsouza@gmail.com>
2022-02-23 20:28:20 -0300

    Create PydanticObjectId class

---
+++ app/models/objectid.py
@@ -0,0 +1,19 @@
+from bson import ObjectId
+from pydantic.json import ENCODERS_BY_TYPE
+
+class PydanticObjectId(ObjectId):
+  @classmethod
+  def __get_validators__(cls):
+    yield cls.validate
+  
+  @classmethod
+  def validate(cls, v):
+    if not isinstance(v, ObjectId):
+      raise ValueError("Not a valid ObjectId")
+    return str(v)
+
+  @classmethod
+  def __modify_schema__(cls, field_schema):
+    field_schema.update(type="string")
+
+ENCODERS_BY_TYPE[PydanticObjectId] = str
