e964e14b825777607c6589678f1ae30bcf87bcb4
Filipe Alves <filipe.alvesdsouza@gmail.com>
2022-02-23 20:34:14 -0300

    Create json encoder

--- app/models/zone.py
+++ app/models/zone.py
@@ -1,7 +1,9 @@
 from pydantic import BaseModel, Field
+from typing import Optional
+from app.models.objectid import PydanticObjectId
 
 class Zone(BaseModel):
-  id: int = Field(alias="_id")
+  id: Optional[PydanticObjectId] = Field(alias="_id")
   name: str
   description: str
   size: float
