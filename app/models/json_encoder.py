611f88aa9e7be30a2df75dffeab7c0fdbe81aed9
Filipe Alves <filipe.alvesdsouza@gmail.com>
2022-03-02 20:22:47 -0300

    Fix datetime conversion

--- app/models/json_encoder.py
+++ app/models/json_encoder.py
@@ -1,8 +1,14 @@
 import json
 from bson import ObjectId
+from datetime import datetime
 
 class JSONEncoder(json.JSONEncoder):
   def default(self, o):
     if isinstance(o, ObjectId):
       return str(o)
-    return json.JSONEncoder.default(self, o)
+
+    elif isinstance(o, datetime):
+      return o.isoformat()
+
+    else:
+      return json.JSONEncoder.default(self, o)
