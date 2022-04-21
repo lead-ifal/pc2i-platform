7627499776307e9ea8587dfa185f9a0248f658a0
Filipe Alves <filipe.alvesdsouza@gmail.com>
2022-04-21 09:55:15 -0300

    Refactor naming

--- app/routes/users/__init__.py
+++ app/routes/users/__init__.py
@@ -16,22 +16,22 @@ users: Collection = database.users
 
 @users_routes.route('/user/new', methods = ['POST'])
 def create():
-  requiredParams = ['name', 'email', 'password']
+  required_params = ['name', 'email', 'password']
   body = request.get_json()
-  includesParams = GlobalController.includesAllRequiredParams(requiredParams, body)
+  includes_params = GlobalController.includes_all_required_params(required_params, body)
 
   try:
-    if includesParams:
-      userExists = UserController.userAlreadyExists(body['email'], users)['exists']
+    if includes_params:
+      user_exists = UserController.user_already_exists(body['email'], users)['exists']
 
-      if userExists:
+      if user_exists:
         raise Exception()
 
-      body['password'] = UserController.encodePassword(body['password'])
+      body['password'] = UserController.encode_password(body['password'])
       user = User(**body)
       users.insert_one(user.dict())
 
-      return GlobalController.generateResponse(
+      return GlobalController.generate_response(
         HTTP_CREATED_CODE,
         SUCCESS_MESSAGE,
         user.dict(exclude={'password'})
@@ -40,33 +40,31 @@ def create():
     raise Exception()
 
   except:
-    return GlobalController.generateResponse(HTTP_BAD_REQUEST_CODE, ERROR_MESSAGE)
+    return GlobalController.generate_response(HTTP_BAD_REQUEST_CODE, ERROR_MESSAGE)
 
 @users_routes.route('/user/login', methods=['POST'])
-def signin():
-  requiredParams = ['email', 'password']
+def read():
+  required_params = ['email', 'password']
   body = request.get_json()
-  includesParams = GlobalController.includesAllRequiredParams(requiredParams, body)
+  includes_params = GlobalController.includes_all_required_params(required_params, body)
 
   try:
-    if includesParams:
-      userData = UserController.userAlreadyExists(body['email'], users)
-      userExists = userData['exists']
+    if includes_params:
+      user_data = UserController.user_already_exists(body['email'], users)
+      user_exists = user_data['exists']
 
-      if userExists:
-        user = User(**userData['data'])
-        encodedPassword = body['password'].encode()
-        savedPassword = user.password
-        passwordIsCorrect = bcrypt.checkpw(encodedPassword, savedPassword)
-        
-        if passwordIsCorrect:
-          return GlobalController.generateResponse(
+      if user_exists:
+        user = User(**user_data['data'])
+        password_is_correct = bcrypt.checkpw(body['password'].encode(), user.password)
+
+        if password_is_correct:
+          return GlobalController.generate_response(
             HTTP_SUCCESS_CODE,
             SUCCESS_MESSAGE,
             user.dict(exclude={'password'})
           )
-          
+
     raise Exception()
 
   except:
-    return GlobalController.generateResponse(HTTP_BAD_REQUEST_CODE, ERROR_MESSAGE)
+    return GlobalController.generate_response(HTTP_BAD_REQUEST_CODE, ERROR_MESSAGE)
