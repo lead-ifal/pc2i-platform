46d1ce9cfb11bfcefe42d7ca94f51ac7630908b4
Filipe Alves <filipe.alvesdsouza@gmail.com>
2022-05-03 18:39:36 -0300

    Add zone listing route

--- app/routes/zones/__init__.py
+++ app/routes/zones/__init__.py
@@ -3,3 +3,4 @@ from app.controllers.zone_controller import ZoneController
 
 zones_bp = Blueprint('zones', __name__)
 zones_bp.route('/new', methods=['POST'])(ZoneController.create)
+zones_bp.route('/user/<user_id>', methods=['GET'])(ZoneController.list)
