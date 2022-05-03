fc691fb38af78df4b4c6f7eef03ceaada8998154
Filipe Alves <filipe.alvesdsouza@gmail.com>
2022-05-03 18:38:09 -0300

    Add user_id attribute to zone model

--- app/constants/required_params.py
+++ app/constants/required_params.py
@@ -4,7 +4,7 @@ required_params = {
     'read': ['email', 'password']
   },
   'zone': {
-    'create': ['name', 'description', 'size']
+    'create': ['name', 'description', 'size', 'user_id']
   },
   'culture': {
     'create': ['zone_id', 'name', 'type', 'planting_date', 'coefficient_et', 'phase', 'geographic_coordinates']
