b80568d583e8899e36a91fc100d39960b140d923
Filipe Alves <filipe.alvesdsouza@gmail.com>
2022-05-05 08:56:57 -0300

    Rename zone to irrigation_zone

--- app/controllers/zone_controller.py
+++ app/controllers/zone_controller.py
@@ -1,28 +1,32 @@
 from flask import request
 from typing import Collection
 from app.controllers.global_controller import GlobalController
-from app.models.zone import Zone
+from app.models.irrigation_zone import IrrigationZone
 from app.constants.status_code import HTTP_BAD_REQUEST_CODE, HTTP_CREATED_CODE, HTTP_SUCCESS_CODE
 from app.constants.response_messages import ERROR_MESSAGE, SUCCESS_MESSAGE
 from app.constants.required_params import required_params
 from app import database
 
-zones: Collection = database.zones
+irrigation_zones: Collection = database.irrigation_zones
 
 class ZoneController:
   def create():
     body = request.get_json()
-    params = required_params['zone']['create']
+    params = required_params['irrigation_zone']['create']
     includes_params = GlobalController.includes_all_required_params(params, body)
 
     try:
       if includes_params:
-        zone = Zone(**body)
-        zone_data = zone.dict(exclude_none=True)
+        irrigation_zone = IrrigationZone(**body)
+        irrigation_zone_data = irrigation_zone.dict(exclude_none=True)
 
-        zones.insert_one(zone_data)
+        irrigation_zones.insert_one(irrigation_zone_data)
 
-        return GlobalController.generate_response(HTTP_CREATED_CODE, SUCCESS_MESSAGE, zone_data)
+        return GlobalController.generate_response(
+          HTTP_CREATED_CODE,
+          SUCCESS_MESSAGE,
+          irrigation_zone_data
+        )
 
       raise Exception()
 
@@ -30,10 +34,10 @@ class ZoneController:
       return GlobalController.generate_response(HTTP_BAD_REQUEST_CODE, ERROR_MESSAGE)
 
   def list(user_id):
-    zone_list = zones.find({ 'user_id': user_id })
+    irrigation_zone_list = irrigation_zones.find({ 'user_id': user_id })
     data = []
 
-    for zone in zone_list:
-      data.append(zone)
+    for irrigation_zone in irrigation_zone_list:
+      data.append(irrigation_zone)
 
     return GlobalController.generate_response(HTTP_SUCCESS_CODE, SUCCESS_MESSAGE, data)
--- app/routes/zones/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-from app.controllers.zone_controller import ZoneController
-
-zones_bp = Blueprint('zones', __name__)
-zones_bp.route('/new', methods=['POST'])(ZoneController.create)
-zones_bp.route('/user/<user_id>', methods=['GET'])(ZoneController.list)
