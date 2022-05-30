d45d15264adb0a5a67b9a0fca9466b4cc825af47
Filipe Alves <filipe.alvesdsouza@gmail.com>
2022-05-30 10:52:00 -0300

    Change all routes to plural

--- app/constants/required_params.py
+++ app/constants/required_params.py
@@ -1,12 +1,12 @@
 required_params = {
-  'user': {
+  'users': {
     'create': ['name', 'email', 'password'],
     'read': ['email', 'password']
   },
-  'irrigation_zone': {
+  'irrigation_zones': {
     'create': ['name', 'description', 'size', 'user_id']
   },
-  'culture': {
+  'cultures': {
     'create': ['irrigation_zone_id', 'name', 'type', 'planting_date', 'coefficient_et', 'phase', 'geographic_coordinates']
   }
 }
