95f6b0a8d24470a804d18a18e76bfa6e6efba3c9
Filipe Alves <filipe.alvesdsouza@gmail.com>
2022-05-30 14:30:24 -0300

    Remove /new endpoint in all routes

--- app/routes/cultures/__init__.py
+++ app/routes/cultures/__init__.py
@@ -2,4 +2,4 @@ from flask import Blueprint
 from app.controllers.culture_controller import CultureController
 
 cultures_bp = Blueprint('cultures', __name__)
-cultures_bp.route('/new', methods=['POST'])(CultureController.create)
+cultures_bp.route('', methods=['POST'])(CultureController.create)
--- app/routes/users/__init__.py
+++ app/routes/users/__init__.py
@@ -2,5 +2,5 @@ from flask import Blueprint
 from app.controllers.user_controller import UserController
 
 users_bp = Blueprint('users', __name__)
-users_bp.route('/new', methods=['POST'])(UserController.create)
+users_bp.route('', methods=['POST'])(UserController.create)
 users_bp.route('/login', methods=['POST'])(UserController.login)
