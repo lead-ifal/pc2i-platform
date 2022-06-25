9d2fb11bc5bb6a8e4104167fd861ab04bb523652
Filipe Alves <filipe.alvesdsouza@gmail.com>
2022-06-25 14:24:25 -0300

    Add irrigation types constants

---
+++ app/constants/irrigation_types.py
@@ -0,0 +1,8 @@
+irrigation_types = {
+  1: 'Irrigação localizada',
+  2: 'Irrigação por aspersão',
+  3: 'Pivot central',
+  4: 'Fertirrigação',
+  5: 'Micro aspersão',
+  6: 'Irrigação de superfície'
+}
