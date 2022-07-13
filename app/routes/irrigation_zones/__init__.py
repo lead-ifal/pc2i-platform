a2c3c520f898821f92fda2298dced7a2603279e6
Felipe <felipe.alencar@ifal.edu.br>
2022-07-13 04:06:10 -0300

    feat: inserção de rota para irrigação manual

--- app/routes/irrigation_zones/__init__.py
+++ app/routes/irrigation_zones/__init__.py
@@ -1,8 +1,9 @@
+import this
 from flask import Blueprint
 from app.controllers.zone_controller import ZoneController
 
 irrigation_zones_bp = Blueprint('irrigation-zones', __name__)
 irrigation_zones_bp.route('', methods=['POST'])(ZoneController.create)
 irrigation_zones_bp.route('', methods=['GET'])(ZoneController.list)
-irrigation_zones_bp.route('/irrigate/<zone_id>', methods=['GET'])(ZoneController.toggle_irrigation)
+irrigation_zones_bp.route('/irrigate/<zone_id>', methods=['GET'])(ZoneController.toggle_irrigation(this))
 irrigation_zones_bp.route('/user/<user_id>', methods=['GET'])(ZoneController.show)
