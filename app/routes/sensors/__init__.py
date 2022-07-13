99481e8986feae9c6590e9df2e201b1284f14051
Felipe <felipe.alencar@ifal.edu.br>
2022-07-13 00:48:30 -0300

    fix: ajuste no controller de sensor

--- app/routes/sensors/__init__.py
+++ app/routes/sensors/__init__.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 from app.controllers.sensor_controller import SensorController
 
-sensors_bp = Blueprint('cultures', __name__)
+sensors_bp = Blueprint('sensors', __name__)
 sensors_bp.route('', methods=['POST'])(SensorController.create)
 sensors_bp.route('<sensor_id>/<value>', methods=['POST'])(SensorController.create_reading)
 sensors_bp.route('/irrigation-zone/<irrigation_zone_id>', methods=['GET'])(SensorController.list)
