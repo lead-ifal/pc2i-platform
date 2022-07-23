dcaeb8f25a2b3afa78e38bedf266499998a3b51f
JoaoViniciusLima <jvlo1@aluno.ifal.edu.br>
2022-07-23 16:30:07 -0300

    add schedule irrigation

--- app/routes/irrigation_zones/__init__.py
+++ app/routes/irrigation_zones/__init__.py
@@ -3,7 +3,7 @@ from app.controllers.zone_controller import ZoneController
 
 irrigation_zones_bp = Blueprint('irrigation-zones', __name__)
 irrigation_zones_bp.route('', methods=['POST'])(ZoneController.create)
-irrigation_zones_bp.route('/schedule_irrigation', methods=['POST'])(ZoneController.schedule_irrigation)
+irrigation_zones_bp.route('/schedule-irrigation', methods=['POST'])(ZoneController.schedule_irrigation)
 irrigation_zones_bp.route('/<zone_id>', methods=['GET'])(ZoneController.show)
 irrigation_zones_bp.route('/irrigate/<zone_id>', methods=['GET'])(ZoneController.toggle_irrigation)
 irrigation_zones_bp.route('/user/<user_id>', methods=['GET'])(ZoneController.list)
