911da2f2478d6e2bb4fc33568cf6f59653d3be43
Felipe <felipe.alencar@ifal.edu.br>
2022-11-07 16:28:56 -0300

    Remoção de print

--- app/middlewares/has_token.py
+++ app/middlewares/has_token.py
@@ -1,6 +1,7 @@
 from functools import wraps
 from flask import request
 from typing import Collection
+from config import Config
 from app.controllers.global_controller import GlobalController
 from app.extensions import database
 from app.constants.status_code import HTTP_UNAUTHORIZED_CODE
@@ -12,13 +13,15 @@ def has_token(route_function):
   @wraps(route_function)
   def main(*args, **kwargs):
     token = request.headers.get('token')
-
-    if token is None:
+    if token is None and Config.DEV_MODE is False:
       return GlobalController.generate_response(HTTP_UNAUTHORIZED_CODE, UNAUTHORIZED_MESSAGE)
 
-    else:
+    elif token is not None or Config.DEV_MODE is True:
       try:
-        count = users.count_documents({ 'token': token })
+        if token is None:
+          count = users.count_documents({'token' : 'dev' })
+        else:  
+          count = users.count_documents({ 'token': token })
 
         if count == 0:
           raise Exception()
