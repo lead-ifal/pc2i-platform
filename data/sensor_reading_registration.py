a5d7a72768c4b649611247294fde55f5c7333c38
João Vinícius Lima <jvlo1@aluno.ifal.edu.br>
2023-07-05 22:58:56 -0300

    adição_do_atributo_context

--- data/sensor_reading_registration.py
+++ data/sensor_reading_registration.py
@@ -5,26 +5,37 @@ import os
 import dotenv
 from bson import ObjectId
 
-
 dotenv.load_dotenv(dotenv.find_dotenv())
 
 sensors_table = database["sensors"]
 sensor_reading_table = database["sensors_readings"]
+culture_table = database["cultures"]
 
 
 class SensorReadingRegistration:
     def TrainCreate():
         train = pd.read_csv("csv_files/Train.csv")
         sensor_names = train.columns.tolist()
+        date = train["timestamp"]
         sensor_names.remove("timestamp")
 
         for sensor_name in sensor_names:
             values = train[sensor_name].tolist()
             sensor = sensors_table.find_one({"name": sensor_name})
+            linha = 0
             for value in values:
                 sensor_id = sensor["_id"]
-                url = os.getenv("URL") + "/" + str(sensor_id) + "/" + str(value)
+                url = (
+                    os.getenv("SENSOR_URL")
+                    + "/"
+                    + str(sensor_id)
+                    + "/"
+                    + str(value)
+                    + "/"
+                    + date[linha]
+                )
                 requests.post(url)
+                linha = linha + 1
 
     def ContextDataPeanutCreate():
         peanut_1_id = ObjectId(os.getenv("PEANUT_1_ID"))
@@ -32,8 +43,8 @@ class SensorReadingRegistration:
         peanut_3_id = ObjectId(os.getenv("PEANUT_3_ID"))
         data_peanuts = pd.read_csv("csv_files/Context_Data_Peanuts.csv")
         sensor_names = data_peanuts.columns.tolist()
+        date = data_peanuts["Date"]
         sensor_names.remove("Date")
-
         for sensor_name in sensor_names:
             values = data_peanuts[sensor_name].tolist()
             sensor_1 = sensors_table.find_one(
@@ -48,26 +59,67 @@ class SensorReadingRegistration:
             sensor_1_id = sensor_1["_id"]
             sensor_2_id = sensor_2["_id"]
             sensor_3_id = sensor_3["_id"]
+            linha = 0
             for value in values:
-                url = os.getenv("URL") + "/" + str(sensor_1_id) + "/" + str(value)
+
+                url = (
+                    os.getenv("SENSOR_URL")
+                    + "/"
+                    + str(sensor_1_id)
+                    + "/"
+                    + str(value)
+                    + "/"
+                    + str(date[linha])
+                )
                 requests.post(url)
-                url = os.getenv("URL") + "/" + str(sensor_2_id) + "/" + str(value)
+                url = (
+                    os.getenv("SENSOR_URL")
+                    + "/"
+                    + str(sensor_2_id)
+                    + "/"
+                    + str(value)
+                    + "/"
+                    + str(date[linha])
+                )
                 requests.post(url)
-                url = os.getenv("URL") + "/" + str(sensor_3_id) + "/" + str(value)
+                url = (
+                    os.getenv("SENSOR_URL")
+                    + "/"
+                    + str(sensor_3_id)
+                    + "/"
+                    + str(value)
+                    + "/"
+                    + str(date[linha])
+                )
                 requests.post(url)
+                linha = linha + 1
 
     def ContextDataMaizeCreate():
         maize_id = ObjectId(os.getenv("MAIZE_ID"))
         data_maize = pd.read_csv("csv_files/Context_Data_Maize.csv")
         sensor_names = data_maize.columns.tolist()
-        sensor_names.remove("Date")
+        context = []
 
-        for sensor_name in sensor_names:
-            values = data_maize[sensor_name].tolist()
-            sensor = sensors_table.find_one(
-                {"name": sensor_name, "culture_id": maize_id}
-            )
-            sensor_id = sensor["_id"]
-            for value in values:
-                url = os.getenv("URL") + "/" + str(sensor_id) + "/" + str(value)
-                requests.post(url)
+        url = os.getenv("CULTURE_URL") + "/" + str(maize_id)
+
+        for index, row in data_maize.iterrows():
+            context_row = []
+
+            for column_name, value in row.items():
+                context_row.append({column_name: str(value)})
+            context.append(context_row)
+
+        culture = culture_table.find_one({"_id": ObjectId(maize_id)})
+
+        culture = {
+            "irrigation_zone_id": str(culture["irrigation_zone_id"]),
+            "name": culture["name"],
+            "type": culture["type"],
+            "planting_date": str(culture["planting_date"]),
+            "coefficient_et": str(culture["coefficient_et"]),
+            "phase": culture["phase"],
+            "geographic_coordinates": culture["geographic_coordinates"],
+            "context": context,
+        }
+
+        requests.patch(url, json=culture)
